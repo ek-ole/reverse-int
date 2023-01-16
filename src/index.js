e51ba958b12d00e1cf1c97bd7e968c921c4ddd6c
ek-ole <ek_34@mail.ru>
2023-01-17 01:08:35 +0300

    init: create reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const numAbs = Math.abs(n)
+    const string = numAbs.toString();
+    const splitString = string.split("");
+  const reverseArray = splitString.reverse();
+  const joinArray = reverseArray.join("");
+  return joinArray;
+
 }
